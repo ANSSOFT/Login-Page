7c88a5d01857198af59ece087c42afbea5aecb6b
fastali <fastali@mail.ru>
2024-01-23 19:03:34 +0300

    Update script.js
    wrapper fixed

--- staj2/script.js
+++ staj2/script.js
@@ -1,13 +1,14 @@
 const signUpBtnLink = document.querySelector('.signUpBtn-link');
 const signInBtnLink = document.querySelector('.signInBtn-link');
-const wrapper = document.querySelector('.wrapper');
+const wrapper_sign_in = document.querySelector('.form-wrapper sign-in');
+const wrapper_sign_up = document.querySelector('.form-wrapper sign-up');
 
 signUpBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
+    if(wrapper_sign_up.classList.contains('active'))
+    {
+        wrapper_sign_in.classList.toggle('active');
+    } else {
+        wrapper_sign_up.classList.toggle('active');
+    }
 
-});
-
-signInBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
-    
 });
